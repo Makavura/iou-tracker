ae102a6a8750036e03fb4d8e587a9589d8cda12c
Makavura <makavuramughanga@gmail.com>
2021-02-04 09:48:32 +0300

    user iou endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,31 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Post, Controller, Delete, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get('/users')
-  fetchUsers(@Body() payload: FetchUsersInformation) {
+  fetchUsers(@Body() payload: FetchUsersInformation, @Res() Response: Response ) {
 
   }
+
+  @Post('/add')
+  createUsers(@Body() payload: User, @Res() Response: Response) {
+
+  }
+
+  @Post('/iou')
+  createIOU(@Body() payload: IOUPayload, @Res() Response: Response) {
+
+  }
+
+  @Delete('/user')
+  deleteUserIOUs(@Body() payload: User, @Res() Response: Response) {
+
+  }
+
 
 }
 
@@ -16,4 +33,14 @@ interface FetchUsersInformation {
   "users": [
     string
   ]
+}
+
+interface IOUPayload {
+  lender: string,
+  borrower: string,
+  amount: number
+}
+
+interface User {
+  user: string
 }
