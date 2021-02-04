b470fc5fcc6f837cb9e2a7caff0b8dcc0ce26048
Makavura <makavuramughanga@gmail.com>
2021-02-04 09:37:40 +0300

    list of user information endpoint and interface of payload

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/users')
+  fetchUsers(@Body() payload: FetchUsersInformation) {
+
   }
+
+}
+
+interface FetchUsersInformation {
+  "users": [
+    string
+  ]
 }
